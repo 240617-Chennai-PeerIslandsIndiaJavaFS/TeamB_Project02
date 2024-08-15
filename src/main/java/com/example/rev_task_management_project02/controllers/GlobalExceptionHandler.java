6edf91173acda0acbe1d684ce2a8568344f3e530
=Anuroop <anuroopks7911@gmail.com>
2024-08-15 12:19:00 +0530

    Adding timestamp functionalities like create, delete, update etc.

--- src/main/java/com/example/rev_task_management_project02/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/example/rev_task_management_project02/controllers/GlobalExceptionHandler.java
@@ -1,45 +1,48 @@
 package com.example.rev_task_management_project02.controllers;
 
 
-import com.example.rev_task_management_project02.exceptions.LoginFailedException;
-import com.example.rev_task_management_project02.exceptions.ProjectNotFoundException;
-import com.example.rev_task_management_project02.exceptions.TaskNotFoundException;
-import com.example.rev_task_management_project02.exceptions.UserNotFoundException;
-import com.example.rev_task_management_project02.exceptions.MilestoneNotFoundException;
+import com.example.rev_task_management_project02.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String>  handleUserNotFoundException(UserNotFoundException ex){
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> handleNullPointerException(NullPointerException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(LoginFailedException.class)
-    public ResponseEntity<String> handleLoginFailedException(LoginFailedException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleLoginFailedException(LoginFailedException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(ProjectNotFoundException.class)
-    public ResponseEntity<String> handleProjectNotFoundException(ProjectNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleProjectNotFoundException(ProjectNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(MilestoneNotFoundException.class)
-    public ResponseEntity<String> handleMilestoneException(MilestoneNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MilestoneNotFoundException.class)
+    public ResponseEntity<String> handleMilestoneNotFoundException(MilestoneNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TimestampNotFoundException.class)
+    public ResponseEntity<String> handleTimestampNotFoundException(TimestampNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
 }
