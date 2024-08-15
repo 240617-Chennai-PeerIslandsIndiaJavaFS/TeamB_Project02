5a9475ace39019e65a040559bc894986a8e19f83
haritha75 <yerukonduharitha@gmail.com>
2024-08-15 17:32:03 +0530

    Updated Task Service File

--- src/main/java/com/example/rev_task_management_project02/services/TaskService.java
+++ src/main/java/com/example/rev_task_management_project02/services/TaskService.java
@@ -3,10 +3,12 @@ package com.example.rev_task_management_project02.services;
 import com.example.rev_task_management_project02.dao.TaskRepository;
 import com.example.rev_task_management_project02.exceptions.TaskNotFoundException;
 import com.example.rev_task_management_project02.models.Task;
+import com.example.rev_task_management_project02.models.TimeStamp;
 import com.example.rev_task_management_project02.utilities.EntityUpdater;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -14,11 +16,15 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
     private final EntityUpdater entityUpdater;
+    private final TimeStampService timeStampService;
+
+
 
     @Autowired
-    public TaskService(TaskRepository taskRepository, EntityUpdater entityUpdater) {
+    public TaskService(TaskRepository taskRepository, EntityUpdater entityUpdater,TimeStampService timeStampService) {
         this.taskRepository = taskRepository;
         this.entityUpdater = entityUpdater;
+        this.timeStampService =  timeStampService;
     }
 
     public Task getTaskById(Long id) throws TaskNotFoundException {
@@ -27,7 +33,21 @@ public class TaskService {
     }
 
     public Task createTask(Task newTask) {
-        return taskRepository.save(newTask);
+        newTask.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        newTask.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+
+        Task savedTask = taskRepository.save(newTask);
+
+        if (savedTask.getMilestone() != null) {
+            TimeStamp timeStamp = new TimeStamp();
+            timeStamp.setTask(savedTask);
+            timeStamp.setMilestone(savedTask.getMilestone());
+            timeStamp.setTimeStamp(new Timestamp(System.currentTimeMillis()));
+
+            timeStampService.createTimeStamp(timeStamp);
+        }
+
+        return savedTask;
     }
 
     public Task updateTask(Long id, Task updatedTask) throws TaskNotFoundException {
@@ -35,8 +55,20 @@ public class TaskService {
                 new TaskNotFoundException("Task with ID " + id + " not found"));
 
         Task updatedTaskEntity = entityUpdater.updateFields(existingTask, updatedTask);
+        updatedTaskEntity.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
 
-        return taskRepository.save(updatedTaskEntity);
+        Task savedTask = taskRepository.save(updatedTaskEntity);
+
+        if (savedTask.getMilestone() != null) {
+            TimeStamp timeStamp = new TimeStamp();
+            timeStamp.setTask(savedTask);
+            timeStamp.setMilestone(savedTask.getMilestone());
+            timeStamp.setTimeStamp(new Timestamp(System.currentTimeMillis()));
+
+            timeStampService.createTimeStamp(timeStamp);
+        }
+
+        return savedTask;
     }
 
     public Task deleteTaskById(Long id) throws TaskNotFoundException {
