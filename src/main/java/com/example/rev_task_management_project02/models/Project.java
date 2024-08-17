e7db1d7f344bb89f3cbd6912666e8ad7510736bc
haritha75 <yerukonduharitha@gmail.com>
2024-08-17 23:15:57 +0530

    Updated-task-model

--- src/main/java/com/example/rev_task_management_project02/models/Project.java
+++ src/main/java/com/example/rev_task_management_project02/models/Project.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 import java.sql.Timestamp;
 import java.util.List;
